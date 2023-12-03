05303c8a6ea8d6881f117a1759236f137e0abf97
Hysenbergg <huseyinsaidzeyrek@gmail.com>
2023-12-03 18:05:13 +0300

    charts library is removed

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import StatisticCard from "../components/statistics/StatisticCard";
-import { Area, Pie } from "@ant-design/plots";
 import Header from "../components/header/Header";
 import { Spin } from "antd";
 
@@ -38,7 +37,65 @@ function StatisticPage() {
       });
   };
 
-  const config = {
+  
+
+  const totalAmount = () => {
+    const amount = data.reduce((total, item) => item.totalAmount + total, 0);
+    return `${amount.toFixed(2)}₺`;
+  };
+
+  return (
+    <>
+      <Header />
+      <h1 className="text-4xl font-bold text-center mb-4">İstatistikler</h1>
+      {data ? (
+        <div className="flex flex-col px-6 md:pb-0 pb-20">
+          <div className="statistic-section">
+            <h2 className="text-lg">
+              Hoş geldin{" "}
+              <span className="text-green-700 text-xl font-bold">
+                {user.username}
+              </span>
+              .
+            </h2>
+            <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 md:gap-10 gap-4 my-5">
+              <StatisticCard
+                title={"Toplam Müşteri"}
+                amount={data?.length}
+                image={"images/user.png"}
+              />
+              <StatisticCard
+                title={"Toplam Kazanç"}
+                amount={totalAmount()}
+                image={"images/money.png"}
+              />
+              <StatisticCard
+                title={"Toplam Satış"}
+                amount={data?.length}
+                image={"images/sale.png"}
+              />
+              <StatisticCard
+                title={"Toplam Ürün"}
+                amount={products.length}
+                image={"images/product.png"}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Spin
+          size="large"
+          className="absolute top-1/2 h-screen w-screen flex justify-center"
+        />
+      )}
+    </>
+  );
+}
+
+export default StatisticPage;
+
+/*
+const config = {
     data,
     xField: "customerName",
     yField: "subTotal",
@@ -83,49 +140,7 @@ function StatisticPage() {
       },
     },
   };
-
-  const totalAmount = () => {
-    const amount = data.reduce((total, item) => item.totalAmount + total, 0);
-    return `${amount.toFixed(2)}₺`;
-  };
-
-  return (
-    <>
-      <Header />
-      <h1 className="text-4xl font-bold text-center mb-4">İstatistikler</h1>
-      {data ? (
-        <div className="flex flex-col px-6 md:pb-0 pb-20">
-          <div className="statistic-section">
-            <h2 className="text-lg">
-              Hoş geldin{" "}
-              <span className="text-green-700 text-xl font-bold">
-                {user.username}
-              </span>
-              .
-            </h2>
-            <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 md:gap-10 gap-4 my-5">
-              <StatisticCard
-                title={"Toplam Müşteri"}
-                amount={data?.length}
-                image={"images/user.png"}
-              />
-              <StatisticCard
-                title={"Toplam Kazanç"}
-                amount={totalAmount()}
-                image={"images/money.png"}
-              />
-              <StatisticCard
-                title={"Toplam Satış"}
-                amount={data?.length}
-                image={"images/sale.png"}
-              />
-              <StatisticCard
-                title={"Toplam Ürün"}
-                amount={products.length}
-                image={"images/product.png"}
-              />
-            </div>
-            <div className="flex justify-between gap-10 lg:flex-row flex-col items-center mb-10">
+ <div className="flex justify-between gap-10 lg:flex-row flex-col items-center mb-10">
               <div className="lg:w-1/2 w-full lg:h-full h-72">
                 <Area {...config} />
               </div>
@@ -133,16 +148,4 @@ function StatisticPage() {
                 <Pie {...config2} />
               </div>
             </div>
-          </div>
-        </div>
-      ) : (
-        <Spin
-          size="large"
-          className="absolute top-1/2 h-screen w-screen flex justify-center"
-        />
-      )}
-    </>
-  );
-}
-
-export default StatisticPage;
+*/
